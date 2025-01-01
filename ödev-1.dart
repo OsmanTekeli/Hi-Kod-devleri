c8bebcafd0925d0f6b6b11798296105d779ae090
Osman Tekeli <165707730+OsmanTekeli@users.noreply.github.com>
2025-01-01 03:23:10 +0300

    Add files via upload

---
+++ ödev-1.dart
@@ -0,0 +1,11 @@
+void main() {
+  String isim = "Osman";
+  String soy_isim = "Tekeli";
+  int Yas = 24;
+  bool ResitMi = Yas >= 18;
+
+  print(isim);
+  print(soy_isim);
+  print(Yas);
+  print(ResitMi);
+}
